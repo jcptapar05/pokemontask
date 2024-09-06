00c84e47f4ab7c02a296949f6b03d1620d1b1ebc
jcptapar05 <jcptapar05@gmail.com>
2024-09-06 16:32:03 +0800

    [Cognith-task] Update test cases

--- src/pages/BoilerplatePage/__tests__/step-definitions/ComponentView.steps.tsx
+++ src/pages/BoilerplatePage/__tests__/step-definitions/ComponentView.steps.tsx
@@ -7,41 +7,40 @@ import ComponentView from "../../ComponentView";
 const screenProps = {};
 
 const feature = loadFeature(
-  "./src/pages/BoilerplatePage/__tests__/features/ComponentView-scenario.feature"
+ "./src/pages/BoilerplatePage/__tests__/features/ComponentView-scenario.feature"
 );
 
 defineFeature(feature, (test) => {
-  beforeEach(() => {
-    jest.resetModules();
+ beforeEach(() => {
+  jest.resetModules();
+ });
+
+ test("User navigates to ComponentView", ({ given, when, then }) => {
+  let ComponentViewWrapper: ShallowWrapper;
+  let instance: ComponentView;
+
+  given("User loading ComponentView Page", () => {
+   ComponentViewWrapper = shallow(<ComponentView {...screenProps} />);
   });
 
-  test("User navigates to ComponentView", ({ given, when, then }) => {
-    let ComponentViewWrapper: ShallowWrapper;
-    let instance: ComponentView;
-
-    given("User loading ComponentView Page", () => {
-      ComponentViewWrapper = shallow(<ComponentView {...screenProps} />);
-    });
-
-    when("User successfully loads ComponentView Page", () => {
-      instance = ComponentViewWrapper.instance() as ComponentView;
-    });
-
-    then("User will see an increment button", () => {
-      const button = ComponentViewWrapper.find('[data-test-id="btn-click"]');
-
-      expect(button.text()).toBe("Increment Number");
-    });
-
-    when("User will click the increment button", () => {
-      const button = ComponentViewWrapper.find('[data-test-id="btn-click"]');
-      button.simulate("click");
-    });
-
-    then("User will see the new value after increment button click", () => {
-      const value = ComponentViewWrapper.find('[data-test-id="value-text"]');
-
-      expect(value.text()).toBe("1");
-    });
+  when("User successfully loads ComponentView Page", () => {
+   instance = ComponentViewWrapper.instance() as ComponentView;
   });
+
+  then("User will see an increment button", () => {
+   const button = ComponentViewWrapper.find('[data-test-id="btn-click"]');
+   expect(button.text()).toBe("Increment Number");
+  });
+
+  when("User will click the increment button", () => {
+   const button = ComponentViewWrapper.find('[data-test-id="btn-click"]');
+   button.simulate("click");
+  });
+
+  then("User will see the new value after increment button click", () => {
+   const value = ComponentViewWrapper.find('[data-test-id="value-text"]');
+
+   expect(value.text()).toBe("1");
+  });
+ });
 });
--- src/pages/Details/__tests__/step-definitions/Details.steps.tsx
+++ src/pages/Details/__tests__/step-definitions/Details.steps.tsx
@@ -1,6 +1,7 @@
 import { shallow, ShallowWrapper } from "enzyme";
 import { defineFeature, loadFeature } from "jest-cucumber";
 import DetailsView from "../../DetailsView";
+import PokemonImg from "../../../../components/PokemonImg";
 
 const mockedProps = {};
 
@@ -20,18 +21,34 @@ defineFeature(feature, (test) => {
    DetailsViewWrapper = shallow(<DetailsView {...mockedProps} />);
    DetailsViewWrapper.setState({
     loading: false,
-    // details: {
-    //  abilities: [{ ability: { name: "overgrow" } }],
-    //  types: [{ type: { name: "grass" } }],
-    //  stats: [{ stat: { name: "speed" }, base_stat: 45 }],
-    // },
+    details: {
+     name: "bulbasaur",
+     abilities: [{ ability: { name: "overgrow" } }],
+     types: [{ type: { name: "grass" } }],
+     stats: [{ stat: { name: "speed" }, base_stat: 45 }],
+    },
    });
    DetailsViewWrapper.update();
   });
 
+  then("User will see the Pokémon's image", () => {
+   const de = shallow(
+    <PokemonImg
+     maxSize={500}
+     id={1}
+    />
+   );
+   expect(de.find("img").exists()).toBe(true);
+  });
+
+  then("User will see the Pokémon's name", () => {
+   const de = DetailsViewWrapper.find('[data-test-id="pokemon-name"]');
+   expect(de.text()).toBe("bulbasaur");
+  });
+
   then("User will see the Pokémon's ability", () => {
    const ability = DetailsViewWrapper.find(".ability").first();
-   expect(ability.text()).toBe("Ability");
+   expect(ability.text()).toContain("Ability");
   });
 
   then("User will see the Pokémon's types", () => {
